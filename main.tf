8e76135a6e102d08c22c4b033c8d927437b89d51
jerin-hc <jerin.johnykutty@hashicorp.com>
2026-07-01 18:02:23 +0530

    added arn

--- main.tf
+++ main.tf
@@ -147,3 +147,17 @@ resource "aws_security_group" "instance_sg" {
     cost-center = "engineering-research-01"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
